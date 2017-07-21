47313c2427cb2d28e8435451b18a5a4d6068ff91
kimmmx <max.kimmet@comed.com>
2017-07-21 15:50:05 -0500

    Added autocorrelation

--- dsp/fft_test.py
+++ dsp/fft_test.py
@@ -2,13 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def autocorrelate(x):
+    result = np.correlate(x, x, mode='full')
+    return result[result.size/2:]
+
+
 Fs = 48.0  # sampling rate
 length = 90
 Ts = 1.0/Fs  # sampling interval
 t = np.arange(0, length, Ts)  # time vector
 
 noise = np.random.random([len(t)]) * 5
-ff = 0.5;   # frequency of the signal
+ff = 5;   # frequency of the signal
 no_signal_length = 60 * 48
 y = [0 for i in range(no_signal_length)]
 for i in t[:int(Fs*length - no_signal_length)]:
@@ -25,12 +30,20 @@ frq = frq[range(n/2)] # one side frequency range
 Y = np.fft.fft(y)/n # fft computing and normalization
 Y = Y[range(n/2)]
 
-fig, ax = plt.subplots(2, 1)
+auto_corr = autocorrelate(y)
+
+Y2 = np.fft.fft(auto_corr)/n
+Y2 = Y2[range(n/2)]
+
+
+fig, ax = plt.subplots(4, 1)
 ax[0].plot(t,y)
 ax[0].set_xlabel('Time')
 ax[0].set_ylabel('Amplitude')
-ax[1].plot(frq,abs(Y),'r') # plotting the spectrum
+ax[1].plot(frq[100:],abs(Y[100:]),'r') # plotting the spectrum
 ax[1].set_xlabel('Freq (Hz)')
 ax[1].set_ylabel('|Y(freq)|')
+ax[2].plot(t, auto_corr)
+ax[3].plot(frq[100:], abs(Y2[100:]), 'r')
 
 plt.show()
